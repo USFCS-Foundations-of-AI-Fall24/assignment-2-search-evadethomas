5d0f5264fec6be0e1d3781b6d5df4efd9c67ee43
evadethomas <112012162+evadethomas@users.noreply.github.com>
2024-09-20 23:30:14 -0700

    Committing little test set up to sepearate tasks question 2

--- mars_planner.py
+++ mars_planner.py
@@ -17,10 +17,11 @@ from copy import deepcopy
 from search_algorithms import breadth_first_search, depth_first_search
 
 class RoverState :
-    def __init__(self, loc="station", sample_extracted=False, holding_sample=False, charged=False):
+    def __init__(self, loc="station", sample_extracted=False, holding_sample=False, holding_tool=False, charged=False):
         self.loc = loc
         self.sample_extracted=sample_extracted
         self.holding_sample = holding_sample
+        self.holding_tool = holding_tool
         self.charged=charged
         self.prev = None
 
@@ -29,14 +30,16 @@ class RoverState :
       return (self.loc == other.loc and
                 self.sample_extracted == other.sample_extracted and
                 self.holding_sample == other.holding_sample and
-                self.charged == other.charged and self.prev == self.prev)
+                self.charged == other.charged and
+                self.holding_tool == other.holding_tool)
 
 
     def __repr__(self):
         return (f"Location: {self.loc}\n" +
                 f"Sample Extracted?: {self.sample_extracted}\n"+
                 f"Holding Sample?: {self.holding_sample}\n" +
-                f"Charged? {self.charged}")
+                f"Charged? {self.charged}\n" +
+                f"Holding Tool? {self.holding_tool}\n")
 
     def __hash__(self):
         return self.__repr__().__hash__()
@@ -59,6 +62,7 @@ def move_to_sample(state) :
     r2.loc = "sample"
     r2.prev=state
     return r2
+
 def move_to_station(state) :
     r2 = deepcopy(state)
     r2.loc = "station"
@@ -72,18 +76,36 @@ def move_to_battery(state) :
     return r2
 # add tool functions here
 
+def pick_up_tool(state) :
+    r2 = deepcopy(state)
+    if state.loc == "station":
+        r2.holding_tool = True
+    r2.prev = state
+    return r2
+
+def drop_tool(state) :
+    r2 = deepcopy(state)
+    if state.loc == "station" and state.holding_tool == True:
+        r2.holding_tool = False
+    r2.prev = state
+    return r2
+
+def use_tool(state) :
+    if state.holding_tool == True:
+        return pick_up_sample(state)
+    return state
 
 def pick_up_sample(state) :
     r2 = deepcopy(state)
     r2.sample_extracted = True
-    if state.sample_extracted and state.loc == "sample":
+    if state.sample_extracted and state.loc == "sample" and state.holding_tool == True:
         r2.holding_sample = True
     r2.prev = state
     return r2
 
 def drop_sample(state) :
     r2 = deepcopy(state)
-    if state.sample_extracted and state.loc == "station":
+    if state.sample_extracted and state.loc == "station" and state.holding_tool == True:
         r2.holding_sample = False
     r2.prev = state
     return r2
@@ -96,7 +118,7 @@ def charge(state) :
     return r2
 
 
-action_list = [charge, drop_sample, pick_up_sample,
+action_list = [charge, drop_sample, pick_up_sample, pick_up_tool, drop_tool, use_tool,
                move_to_sample, move_to_battery, move_to_station]
 
 def battery_goal(state) :
@@ -104,19 +126,45 @@ def battery_goal(state) :
 ## add your goals here.
 
 def mission_complete(state) :
-    return state.loc == "battery" and state.charged == True and state.sample_extracted == True and state.holding_sample == False
+    return state.loc == "battery" and state.charged == True and state.sample_extracted == True and state.holding_sample == False and state.holding_tool == False
     
 
 
 if __name__=="__main__" :
     s = RoverState()
-    
-    bfs_result, bfs_count = breadth_first_search(s, action_list, mission_complete)
-    print("BFS state count: ", bfs_count)
-    print("Final BFS result: ", bfs_result)
 
-    dfs_result, dfs_count = depth_first_search(s, action_list, mission_complete)
-    print("DFS state count: ", dfs_count)
-    print("Final DFS result: ", dfs_result)
+    print("Results!! :")
+
+    print("Mission complete: \n")
+
+    try:
+        bfs_result, bfs_count = breadth_first_search(s, action_list, mission_complete)
+        print("BFS state count: ", bfs_count)
+        print("Final BFS result: ", bfs_result)
+    except TypeError:
+        print("No solution found for bfs")
+
+    print("Finding dfs limit...")
+
+    try:
+        dfs_result, dfs_count = depth_first_search(s, action_list, mission_complete)
+        print("DFS state count: ", dfs_count)
+        print("Final DFS result: ", dfs_result)
+    except TypeError:
+        print("No solution found for dfs, adding to limit")
+    
+    print("using to find good limit to speed up testing... ")
+    limit = 1
+
+    while (limit < dfs_count):
+        try:
+            dfs_result, dfs_count = depth_first_search(s, action_list, mission_complete, limit=limit)
+            print("DFS state count: ", dfs_count)
+            print("Final DFS result: ", dfs_result)
+            print("Limit fed to get solution: ", limit)
+            break
+        except TypeError:
+            print("No solution found for dfs, adding to limit")
+            limit += 1
 
 
--- search_algorithms.py
+++ search_algorithms.py
@@ -40,10 +40,12 @@ def breadth_first_search(startState, action_list, goal_test, use_closed_list=Tru
 ### Note the similarity to BFS - the only difference is the search queue
 
 ## use the limit parameter to implement depth-limited search
-def depth_first_search(startState, action_list, goal_test, use_closed_list=True,limit=0) :
+def depth_first_search(startState, action_list, goal_test, use_closed_list=True, limit=0) :
     search_queue = deque()
     closed_list = {}
     count = 0
+    depth_count = 0
+    print("Limit ",limit)
 
     search_queue.append((startState,""))
     if use_closed_list :
@@ -51,6 +53,8 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
     while len(search_queue) > 0 :
         ## this is a (state, "action") tuple
         next_state = search_queue.pop()
+        depth_count += 1
+
         count += 1
         if goal_test(next_state[0]):
             print("Goal found")
@@ -61,6 +65,9 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
                 print(ptr)
             return next_state, count
         else :
+            if depth_count == limit:
+                continue
+
             successors = next_state[0].successors(action_list)
             if use_closed_list :
                 successors = [item for item in successors
@@ -70,7 +77,3 @@ def depth_first_search(startState, action_list, goal_test, use_closed_list=True,
             search_queue.extend(successors)
 
 ## add iterative deepening search here
-
-
-
-f
