76eb19f3169f90e2d0f06d33ac4d42dd09c4f067
evadethomas <112012162+evadethomas@users.noreply.github.com>
2024-09-23 15:07:04 -0700

    Committing with correct A*

--- routefinder.py
+++ routefinder.py
@@ -39,7 +39,6 @@ def a_star(start_state, heuristic_fn, goal_test, use_closed_list=True) :
     print("start: ", start.f)
 
     search_queue = PriorityQueue()
-    open_list = {}
     closed_list = {}
     
     mars_graph = read_mars_graph("marsmap.txt")
@@ -51,20 +50,19 @@ def a_star(start_state, heuristic_fn, goal_test, use_closed_list=True) :
         current_state = search_queue.get()
 
         if goal_test(current_state) == True :
-
+            
             while current_state.prev_state.location != start_state:
                 print(current_state.prev_state)
                 current_state = current_state.prev_state
-            
             print("prev_state", current_state.prev_state)
             return current_state
 
         for i in mars_graph[current_state.location] :
             i_state = map_state(i, current_state, current_state.g + 1, heuristic_fn(i))
 
-            if i_state.location in closed_list and (i_state.g >= closed_list[i_state.location].g == True) :
-                 continue
-            
+            if i_state.location in closed_list:
+                continue
+
             search_queue.put(i_state)
 
         closed_list[current_state.location] = current_state
