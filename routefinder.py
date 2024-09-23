00524a19ef3a61d8707ea1d55908d91ef724c46c
evadethomas <112012162+evadethomas@users.noreply.github.com>
2024-09-23 14:13:05 -0700

    A* mostly added

--- routefinder.py
+++ routefinder.py
@@ -5,7 +5,7 @@ class map_state() :
     ## f = total estimated cost
     ## g = cost so far
     ## h = estimated cost to goal
-    def __init__(self, location="", mars_graph=None,
+    def __init__(self, location="",
                  prev_state=None, g=0,h=0):
         self.location = location
         self.prev_state = prev_state
@@ -34,29 +34,50 @@ class map_state() :
 
 def a_star(start_state, heuristic_fn, goal_test, use_closed_list=True) :
     map = map_state()
-    start = map_state(start_state, None, 0, sld(start_state))
+    start = map_state(start_state, None, 0, heuristic_fn(start_state))
 
     print("start: ", start.f)
 
     search_queue = PriorityQueue()
-    
+    open_list = {}
     closed_list = {}
     
-    map.mars_graph = read_mars_graph("marsmap.txt")
+    mars_graph = read_mars_graph("marsmap.txt")
     search_queue.put(start)
-    print(map.mars_graph)
+    print(mars_graph)
 
-    # while search_queue.empty() == False :
-    #     for i in search_queue :
+    while search_queue.empty() == False :
+
+        current_state = search_queue.get()
+
+        if goal_test(current_state) == True :
+
+            while current_state.prev_state.location != start_state:
+                print(current_state.prev_state)
+                current_state = current_state.prev_state
             
+            print("prev_state", current_state.prev_state)
+            return current_state
 
+        for i in mars_graph[current_state.location] :
+            i_state = map_state(i, current_state, current_state.g + 1, heuristic_fn(i))
+
+            if i_state.location in closed_list and (i_state.g >= closed_list[i_state.location].g == True) :
+                 continue
+            
+            search_queue.put(i_state)
+
+        closed_list[current_state.location] = current_state
+        
+
+    
     # starting nodes
         # h(n) - open list contains all
         # f(n) - 
 
     ## you do the rest.
-def goal_test(current_state, goal_state) :
-    return current_state == goal_state
+def goal_test(current_state) :
+    return current_state.location == "1,1"
 
 ## default heuristic - we can use this to implement uniform cost search
 def h1(state) :
@@ -78,5 +99,7 @@ def read_mars_graph(filename):
             loc = line.split(':')
             mars_graph[loc[0]] = loc[1].strip().split()
     return mars_graph
+    
 
-a_star("8,8", sld, goal_test, use_closed_list=True)
+final_state = "A-star: ", a_star("8,8", sld, goal_test, use_closed_list=True)
+print(final_state)
